13c21305450bc9562c41b316dd59b46fb951a786
riptxde <riptxdedev@gmail.com>
2025-11-27 00:58:40 -0600

    fix size

--- src/components/ui/file-tree/File.vue
+++ src/components/ui/file-tree/File.vue
@@ -5,7 +5,7 @@
         :disabled="!isSelectable"
         :class="[
             cn(
-                'flex w-fit items-center gap-1.5 rounded-sm pr-1 text-[0.925rem] duration-200 ease-in-out rtl:pl-1 rtl:pr-0',
+                'flex w-fit items-center gap-1.5 rounded-sm pr-1 text-[0.9rem] duration-200 ease-in-out rtl:pl-1 rtl:pr-0',
                 isSelected && isSelectable ? 'bg-muted' : '',
                 isSelectable
                     ? 'cursor-pointer'
