e68c510526244b3f5aacf79e8eb2172e50bec45e
riptxde <riptxdedev@gmail.com>
2025-11-27 01:23:44 -0600

    add tooltips to dock icons

--- src/components/DockArea.vue
+++ src/components/DockArea.vue
@@ -2,25 +2,64 @@
 import { Dock, DockIcon } from "@/components/ui/dock";
 import { Play, Eraser, FolderOpen, Save } from "lucide-vue-next";
 import DockLiquidGlass from "@/components/DockLiquidGlass.vue";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
 </script>
 
 <template>
     <div class="flex justify-center items-center py-4">
-        <DockLiquidGlass>
-            <Dock class="!m-0">
-                <DockIcon>
-                    <Play class="size-5" />
-                </DockIcon>
-                <DockIcon>
-                    <Eraser class="size-5" />
-                </DockIcon>
-                <DockIcon>
-                    <FolderOpen class="size-5" />
-                </DockIcon>
-                <DockIcon>
-                    <Save class="size-5" />
-                </DockIcon>
-            </Dock>
-        </DockLiquidGlass>
+        <TooltipProvider>
+            <DockLiquidGlass>
+                <Dock class="!m-0">
+                    <Tooltip>
+                        <TooltipTrigger as-child>
+                            <DockIcon>
+                                <Play class="size-5" />
+                            </DockIcon>
+                        </TooltipTrigger>
+                        <TooltipContent :side-offset="-15">
+                            <p>Execute</p>
+                        </TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                        <TooltipTrigger as-child>
+                            <DockIcon>
+                                <Eraser class="size-5" />
+                            </DockIcon>
+                        </TooltipTrigger>
+                        <TooltipContent :side-offset="-15">
+                            <p>Clear</p>
+                        </TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                        <TooltipTrigger as-child>
+                            <DockIcon>
+                                <FolderOpen class="size-5" />
+                            </DockIcon>
+                        </TooltipTrigger>
+                        <TooltipContent :side-offset="-15">
+                            <p>Open Script</p>
+                        </TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                        <TooltipTrigger as-child>
+                            <DockIcon>
+                                <Save class="size-5" />
+                            </DockIcon>
+                        </TooltipTrigger>
+                        <TooltipContent :side-offset="-15">
+                            <p>Save</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </Dock>
+            </DockLiquidGlass>
+        </TooltipProvider>
     </div>
 </template>
