82ed7e2d81d26492a13060a62b8457b061281f85
riptxde <riptxdedev@gmail.com>
2025-12-04 00:17:20 -0600

    fix editor pane default sizes

--- src/features/editor/components/EditorWorkspace/EditorWorkspace.vue
+++ src/features/editor/components/EditorWorkspace/EditorWorkspace.vue
@@ -12,14 +12,14 @@ import FileExplorer from "./FileExplorer/FileExplorer.vue";
     <div class="flex-1 overflow-hidden pt-4 px-4">
         <ResizablePanelGroup direction="horizontal" class="h-full gap-4">
             <!-- Script Editor -->
-            <ResizablePanel :default-size="70" :min-size="30">
+            <ResizablePanel :default-size="72.5" :min-size="30">
                 <CodeEditor />
             </ResizablePanel>
 
             <ResizableHandle with-handle />
 
             <!-- File Tree Panel -->
-            <ResizablePanel :default-size="25" :min-size="20">
+            <ResizablePanel :default-size="27.5" :min-size="20">
                 <FileExplorer />
             </ResizablePanel>
         </ResizablePanelGroup>
